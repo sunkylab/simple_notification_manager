89b08d09f9937dddcc2b388c51edfeaede7e266c
Software Development 3line <softwaredev@3lineng.com>
2020-08-10 00:49:17 +0100

    created application
    specified application libraries

---
+++ src/test/java/com/interswitch/notification/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.interswitch.notification;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+
+
+
+}
