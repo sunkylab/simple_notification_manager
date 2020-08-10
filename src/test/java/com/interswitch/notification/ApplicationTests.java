5cd5fa312495c96de90a6f44bc960cbfd91d542c
Software Development 3line <softwaredev@3lineng.com>
2020-08-10 01:03:57 +0100

    added some test cases to be implemented against

--- src/test/java/com/interswitch/notification/ApplicationTests.java
+++ src/test/java/com/interswitch/notification/ApplicationTests.java
@@ -1,7 +1,13 @@
 package com.interswitch.notification;
 
 
+import com.interswitch.notification.application.notification.dto.NotificationDTO;
+import com.interswitch.notification.application.notification.entity.MessageStatus;
+import com.interswitch.notification.application.notification.service.NotificationService;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,6 +16,101 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
+    @Autowired
+    NotificationService notificationService;
 
+    @Test
+    public void whenSmsNotificationIsSent() {
+        // given
+        NotificationDTO notificationDTO = new NotificationDTO();
+        notificationDTO.setMessage("Hi Ola");
+        notificationDTO.setRecipient("2348169141091");
+        notificationDTO.setType("SMS");
+        notificationDTO.setRequestRef("r-001");
+        // when
+        notificationDTO = notificationService.sendNotification(notificationDTO);
+        // then
+        Assert.assertTrue("Notification would has been sent ",notificationDTO.getStatus().equals(MessageStatus.DELIVERED));
+
+    }
+
+    @Test
+    public void whenEmailNotificationIsSent() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void whenNotificationFails() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void whenFailedNotificationIsRePushed() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void whenDeliveredNotificationIsRePushed() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void whenInvalidRequestRefIsUsed() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void whenValidRequestRefIsUsed() {
+        // given
+
+        // when
+
+        // then
+    }
+
+
+    @Test
+    public void whenNotificationIsFetchedWithFilter() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void whenNotificationIsFetchedWithoutFilter() {
+        // given
+
+        // when
+
+        // then
+
+    }
 
 }
